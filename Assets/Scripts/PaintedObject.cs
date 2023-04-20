34dc117cd2da14ad2de2a8d5e0486838c012539b
AdrianCV <sirmuffinhead@gmail.com>
2023-04-20 22:40:11 +0200

    Real Correct material added

--- Assets/Scripts/PaintedObject.cs
+++ Assets/Scripts/PaintedObject.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PaintedObject : MonoBehaviour
 {
     public Material CorrectMat;
+    [SerializeField] Material _realCorrectMat;
     public bool IsCorrect;
 
     private void Update()
@@ -16,6 +17,11 @@ public class PaintedObject : MonoBehaviour
         else
         {
             IsCorrect = false;
+        }
+
+        if (IsCorrect)
+        {
+            GetComponent<Renderer>().sharedMaterial = _realCorrectMat;
         }
     }
 }
